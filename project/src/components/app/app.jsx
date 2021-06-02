2de94fa014e22fb9c9e345a77061de89cb41c730
Bronepuh <darckday2007@mail.ru>
2021-06-02 23:16:00 +0300

    создал компонент главного экрана, компонент карточки, поправил тест, чтобы он не ругался

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import WelcomeScreen from '../wellcome-screen/wellcome-screen';
 
-function App() {
-  return <p>Hello, world!</p>;
+
+function App(props) {
+  const {cardsCount} = props;
+
+  return (
+    <WelcomeScreen cardsCount={cardsCount}/>
+  );
 }
+
+App.propTypes = {
+  cardsCount: PropTypes.number,
+};
 
 export default App;
